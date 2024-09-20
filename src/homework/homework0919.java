eac0eee379146298ff8e643c93faff07558a9f15
lyiceshiki <lyice1067562785@outlook.com>
2024-09-20 17:17:45 +0900

    20240920

--- src/homework/homework0919.java
+++ src/homework/homework0919.java
@@ -5,7 +5,11 @@ public class homework0919 {
     public static void main(String...args) {
     //regexExample1();
     //regexMatch();
-    regexMatch1();
+    //regexMatch1();
+    //lookingAt();
+    //replace();
+    //appendReTa();
+    checkTel("12134567890");
     }
 
     private static void regexExample1() {
@@ -54,12 +58,66 @@ public class homework0919 {
             System.out.println("start(): " + m.start());
             System.out.println("end(): " + m.end());
         }
+    }
 
+    private static void lookingAt(){
+        String regex = "qwer";
+        String input = "qwerrrrrrrr";
+        String input1 = "qqqqwerrrrrrrr";
+        Pattern pattern;
+        Matcher matcher;
+        Matcher matcher1;
 
-        
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(input);
+        matcher1 = pattern.matcher(input1);
+
+        System.out.println("Current regex is : " + regex);
+        System.out.println("Current input is : " + input);
+        System.out.println("Current input1 is : " + input1);
+
+        //lookingAtは部分一致
+        System.out.println("lookingAt():" + matcher.lookingAt());
+        //lookingAtは第一文字から比較する
+        System.out.println("lookingAt():" + matcher1.lookingAt());
+        //matchは全部一致
+        System.out.println("matches(): "+matcher.matches());
+        //
+        System.out.println("matches(): "+matcher1.matches());
+    }
+
+    private static void replace() {
+        String regex = "dog";
+        String input = "The dog says meow. " + "All dogs say meow.";
+        String replace = "cat";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        input = m.replaceAll(replace);
+        System.out.println(input);
 
     }
 
+    private static void appendReTa() {
+        String regex = "a*b";
+        String input = "aaabqooabbdatebjkl";
+        String replace = "-";
 
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        StringBuffer sb = new StringBuffer();
+        while(m.find()) {
+            m.appendReplacement(sb, replace);
+
+        }
+        m.appendTail(sb);
+        System.out.println(sb.toString());
+
+    }
+
+    private static void checkTel(String tel){
+        String regex = "[0-9]{9,11}";
+        System.out.println(tel.matches(regex)?"legal tel":"illegal tel");
+    }
 
 }
